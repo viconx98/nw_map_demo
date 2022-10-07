3062e52a019d4d6a215be12948da911b85dcb7f1
Vihar <vihar1998x@gmail.com>
2022-10-07 23:40:58 +0530

    Add better default tile Change default position

--- script.js
+++ script.js
@@ -1,33 +1,7 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([20, 20], 4);
 
-L.tileLayer('http://localhost:5500/tiles/{z}/{x}/{y}.png', {
+L.tileLayer('https://raw.githubusercontent.com/viconx98/nw_map_demo/master/tiles/{z}/{x}/{y}.png', {
     maxZoom: 6,
     minZoom: 0,
-    errorTileUrl: "http://localhost:5500/tiles/no_tile.png"
+    errorTileUrl: "https://raw.githubusercontent.com/viconx98/nw_map_demo/master/tiles/no_tile.png"
 }).addTo(map);
-
-
-// var marker = L.marker([51.5, -0.09]).addTo(map);
-
-// var circle = L.circle([51.508, -0.11], {
-//     color: 'red',
-//     fillColor: '#f03',
-//     fillOpacity: 0.5,
-//     radius: 500
-// }).addTo(map);
-
-// var polygon = L.polygon([
-//     [51.509, -0.08],
-//     [51.503, -0.06],
-//     [51.51, -0.047]
-// ]).addTo(map);
-
-// marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
-// circle.bindPopup("I am a circle.");
-// polygon.bindPopup("I am a polygon.");
-
-
-// var popup = L.popup()
-//     .setLatLng([51.513, -0.09])
-//     .setContent("I am a standalone popup.")
-//     .openOn(map);
